0cfc1bacda4cbb7a159d95e5ed53b1b6119ef484
sakshi9022 <127666119+sakshi9022@users.noreply.github.com>
2023-03-14 19:55:22 +0530

    Add files via upload

---
+++ Main (5).java
@@ -0,0 +1,13 @@
+class E5{
+          public static void main(String[] args){
+    
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(4);
+        System.out.println(8);
+        System.out.println(16);
+        System.out.println(32);
+        System.out.println(64);
+        
+    }
+}
