8816a15643a65d6defc2caddda831d491cb9c24c
sakshi9022 <127666119+sakshi9022@users.noreply.github.com>
2023-03-15 00:00:07 +0530

    Add files via upload

---
+++ ArrayDemoc.java
@@ -0,0 +1,14 @@
+class ArrayDemoc{
+	public static void main(String[] args) {
+		int[] arr;
+		arr=new int[4];
+		arr[0]=10;
+		arr[1]=20;
+		arr[2]=30;
+		arr[3]=40;
+		
+		 for (int i=0;i<arr.length ;i++ ) {
+		 	System.out.println("Elements at index "+i+" : "+arr[i]);
+		}
+	}
+}
