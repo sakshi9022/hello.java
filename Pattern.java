bdb8155c09b42c679c2614596b7b28efcbddba07
sakshi9022 <127666119+sakshi9022@users.noreply.github.com>
2023-03-14 23:40:30 +0530

    Add files via upload

---
+++ Pattern.java
@@ -0,0 +1,10 @@
+class Pattern{
+	public static void main(String[] args) {
+		for (int i=1;i<=12 ;++i ) {
+			for (int j=1;j<=12 ;++j ) {
+				System.out.print(i);
+			}
+			System.out.println();
+		}
+	}
+}
