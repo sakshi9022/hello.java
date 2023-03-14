94f6ff14fe5b0a0cf23d0aba9954cc96a72b91e6
sakshi9022 <127666119+sakshi9022@users.noreply.github.com>
2023-03-14 23:52:44 +0530

    Add files via upload

---
+++ IncreasingTri.java
@@ -0,0 +1,12 @@
+class IncreasingTri{
+	public static void main(String[] args) {
+		int n=2;
+		for (int i=2;i<=4 ;++i ) {
+			for (int j=2;j<=i ;++j ) {
+				System.out.print(n);
+				n++;
+			}
+		System.out.println();
+		}
+	}
+}
