d4ea3336c85f7031d96b9d6e9c80ddf9ee1189e0
sakshi9022 <127666119+sakshi9022@users.noreply.github.com>
2023-03-15 00:02:58 +0530

    Add files via upload

---
+++ ArrayDemoc1.java
@@ -0,0 +1,10 @@
+class ArrayDemoc1{
+	public static void main(String[] args) {
+		double num[]={3.4,7.1,11,19.8,56.8,5.3};
+		double res=0;
+		 for (int i=0;i<6 ;++i ) {
+		 	res=res+num[i];
+		 }
+		System.out.print("Sum of array is "+res);
+	}
+}
