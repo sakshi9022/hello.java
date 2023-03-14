6f37454cb188030705910fa02d817002e6c5df36
sakshi9022 <127666119+sakshi9022@users.noreply.github.com>
2023-03-14 20:12:35 +0530

    Add files via upload

---
+++ explicit.java
@@ -0,0 +1,19 @@
+package implicit;
+
+public class explicit {
+
+	public static void main(String[] args) {
+		{
+			double d=200.06;
+			long l=(long)d;//explicit type casting
+			int i=(int)l;//explicit type casting 
+			System.out.println("Double Data type value"+d);
+			//fractional part lost
+			System.out.println(" Long Data type value"+l);
+			//fractional part lost 
+			System.out.println("IntData type value"+l);
+		}
+
+	}
+
+}
